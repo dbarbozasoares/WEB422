e0c0a93c72ca976e2a17ab898148ecae1b473cac
dbarbozasoares <dbarboza.soares@gmail.com>
2025-04-04 20:38:26 -0400

    Commiting Test05

--- Test05/square.js
+++ Test05/square.js
@@ -1,9 +1,21 @@
-// square.js
+/********************************************************************************
+ * WEB422 – Test 05
+ *
+ * I declare that this assignment is my own work in accordance with Seneca's
+ * Academic Integrity Policy:
+ * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+ *
+ * Name: Diego B Soares Student ID: ____145820239____ Date: __Apr - 04 - 2025_______
+ * Published URL: _________________https://github.com/dbarbozasoares/WEB422/tree/main__________________________________________
+ ********************************************************************************/
+
+// regular square function
 function square(n) {
   if (n === 0) return 1;
   return n * n;
 }
 
+// square function throwing error for negative numbers
 function squareWithError(n) {
   if (n < 0) {
     throw new Error("Input must be non-negative");
